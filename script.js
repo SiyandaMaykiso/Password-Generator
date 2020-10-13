7184fe2924df2fa1e0fad123350ff7f366f6db6a
Iamsiyanda <s.mayekiso@gmail.com>
2020-10-13 16:20:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,142 @@
-// Variable Commands
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var myInput = document.getElementById("password");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
 
-// Below is the password input
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Var To Upper Case ------------
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
+//--------------------------------------
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  // Save Passoword Function
-
-  function copy2Clip() {
-    inputBoxEl.select();
-    document.execCommand("Save");
-    alert("Successfully saved to Desktop!");
-
   passwordText.value = password;
 
-// Below when the user clicks on the password field, this message box will show
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
+}
 
-
-// Listener Buttons
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Start Function
+function generatePassword() {
+  // Ask four user Input
+  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + passwordLength);
+  
+  if(!passwordLength) {
+    alert("Required value");
 
-  // When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must choose between 8 and 128");
+    console.log("Password length " + passwordLength);
+  
+  } else { 
+    confirmLower = confirm("Will this contain lower case letters?");
+    console.log("Lower case " + confirmLower);
+    confirmUpper = confirm("Will this contain upper case letters?");
+    console.log("Upper case " + confirmUpper);
+    confirmNumber = confirm("Will this contain numbers?");
+    console.log("Number " + confirmNumber);
+    confirmSpecial = confirm("Will this contain special characters?");
+    console.log("Special Character " + confirmSpecial);
 
+  };
 
-// When the user starts to type something inside the password field
-myInput.onkeyup = function()
+  // No answer then
+  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    userChoices = alert("You must choose a criteria");
+  // 4 true options
+  } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = lowerCase.concat(upperCase, numbers, special);
+    console.log(userChoices);
+  }
+  // 3 true options
+  else if (confirmLower && confirmUpper && confirmNumber) {
+    userChoices = lowerCase.concat(upperCase, numbers);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmUpper && confirmSpecial) {
+    userChoices = lowerCase.concat(upperCase, special);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmNumber && confirmSpecial) {
+    userChoices = lowerCase.concat(numbers, special);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = upperCase.concat(numbers, special);
+    console.log(userChoices);
+  }
+  // 2 true options
+  else if (confirmLower && confirmUpper) {
+    userChoices = lowerCase.concat(upperCase);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmNumber) {
+    userChoices = lowerCase.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (confirmLower && confirmSpecial) {
+    userChoices = lowerCase.concat(special);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmNumber) {
+    userChoices = upperCase.concat(numbers);
+    console.log(userChoices);
+  }
+  else if (confirmUpper && confirmSpecial) {
+    userChoices = upperCase.concat(special);
+    console.log(userChoices);
+  }
+  else if (confirmNumber && confirmSpecial) {
+    userChoices = numbers.concat(special);
+    console.log(userChoices);
+  }
+  // 1 true option
+  else if (confirmLower) {
+    userChoices = lowerCase;
+    console.log(userChoices);
+  }
+  else if (confirmUpper) {
+    userChoices = blankUpper.concat(upperCase);
+    console.log(userChoices);
+  }
+  else if (confirmNumber) {
+    userChoices = numbers;
+    console.log(userChoices);
+  }
+  else if (confirmSpecial) {
+    userChoices = special;
+    console.log(userChoices);
+  };
 
+  // Empty variable for the password lenght
+  var passwordBlank = [];
+  
+  // Loop for random selection
+  for (var i = 0; i < passwordLength; i++) {
+    var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+    passwordBlank.push(allChoices);
+    console.log(allChoices);
+  }
 
-};
+  // Join and return the password 
+  var password = passwordBlank.join("");
+  console.log("Your Pasword is: " + password);
+  return password;
+  
+}
