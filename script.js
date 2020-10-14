c2d0968b7ff3c92c5b5d8c0faa9d4972b65e89dc
Iamsiyanda <s.mayekiso@gmail.com>
2020-10-14 13:51:49 +0200

    Major Updates

--- script.js
+++ script.js
@@ -1,144 +1,147 @@
-// Command Functions
-var generateBtn = document.querySelector("#generate");
 
-var passwordLength;
-var confirmLower;
-var confirmUpper;
-var confirmNumber;
-var confirmSpecial;
-var userChoices;
+const alphaLower ="abcdefghijklmnopqrstuvwxyz";
+const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const special = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+const numerical = "012345689";
+let passwordGenerated = ''; 
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Control functions
-var blankUpper = [];
-var toUpper = function (x) {
-  return x.toUpperCase();
-};
-upperCase = lowerCase.map(toUpper);
-// Command Numerals 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-// Input for user to add password
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// User listener for response
-
-document.getElementById("myBtn").addEventListener("click", function() {
+document.getElementById("gen").addEventListener("click", function(){ 
+    genPassword(); 
+    document.getElementById("password").innerHTML = passwordGenerated;
 });
 
-// Begin process
-function generatePassword() {
-  // Respond to the user
-  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
-  console.log("Password length " + passwordLength);
-  
-  if(!passwordLength) {
-    alert("Required value");
+function copyPassword(){
+    let copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Password copied to your clipboard."); 
+}; 
 
-  } else if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("You must choose between 8 and 128");
-    console.log("Password length " + passwordLength);
-  
-  } else { 
-    confirmLower = confirm("Will this contain lower case letters?");
-    console.log("Lower case " + confirmLower);
-    confirmUpper = confirm("Will this contain upper case letters?");
-    console.log("Upper case " + confirmUpper);
-    confirmNumber = confirm("Will this contain numbers?");
-    console.log("Number " + confirmNumber);
-    confirmSpecial = confirm("Will this contain special characters?");
-    console.log("Special Character " + confirmSpecial);
+function genPassword() {
+    
+    let passwordOptions ='';
+    let passwordCheck= []; 
+    
+//user prompt to determine password lenght
+    alert("Welcome to the Next-Gen Password Generator!")
+    let numberChar = prompt("How many characters would you like in your password? It must be between 8 and 128.");
+    
 
-  };
 
-  // If Incorrect
-  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
-    userChoices = alert("You must choose a criteria");
-  // If Correct
-  } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    userChoices = lowerCase.concat(upperCase, numbers, special);
-    console.log(userChoices);
-  }
-  // Correct Options
-  else if (confirmLower && confirmUpper && confirmNumber) {
-    userChoices = lowerCase.concat(upperCase, numbers);
-    console.log(userChoices);
-  }
-  else if (confirmLower && confirmUpper && confirmSpecial) {
-    userChoices = lowerCase.concat(upperCase, special);
-    console.log(userChoices);
-  }
-  else if (confirmLower && confirmNumber && confirmSpecial) {
-    userChoices = lowerCase.concat(numbers, special);
-    console.log(userChoices);
-  }
-  else if (confirmUpper && confirmNumber && confirmSpecial) {
-    userChoices = upperCase.concat(numbers, special);
-    console.log(userChoices);
-  }
-  // Further Combinations
-  else if (confirmLower && confirmUpper) {
-    userChoices = lowerCase.concat(upperCase);
-    console.log(userChoices);
-  }
-  else if (confirmLower && confirmNumber) {
-    userChoices = lowerCase.concat(numbers);
-    console.log(userChoices);
-  }
-  else if (confirmLower && confirmSpecial) {
-    userChoices = lowerCase.concat(special);
-    console.log(userChoices);
-  }
-  else if (confirmUpper && confirmNumber) {
-    userChoices = upperCase.concat(numbers);
-    console.log(userChoices);
-  }
-  else if (confirmUpper && confirmSpecial) {
-    userChoices = upperCase.concat(special);
-    console.log(userChoices);
-  }
-  else if (confirmNumber && confirmSpecial) {
-    userChoices = numbers.concat(special);
-    console.log(userChoices);
-  }
-  // If Correct
-  else if (confirmLower) {
-    userChoices = lowerCase;
-    console.log(userChoices);
-  }
-  else if (confirmUpper) {
-    userChoices = blankUpper.concat(upperCase);
-    console.log(userChoices);
-  }
-  else if (confirmNumber) {
-    userChoices = numbers;
-    console.log(userChoices);
-  }
-  else if (confirmSpecial) {
-    userChoices = special;
-    console.log(userChoices);
-  };
+    while (numberChar < 8 || numberChar > 128 || isNaN(numberChar)) {
+        alert("You must enter a number between 8 and 128");
+        numberChar = prompt("How many characters would you like in your password? It must be between 8 and 128.");
+      
 
-  // If passoword is too long
-  var passwordBlank = [];
-  
-  // Rebuild
-  for (var i = 0; i < passwordLength; i++) {
-    var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
-    passwordBlank.push(allChoices);
-    console.log(allChoices);
-  }
+    }
 
-  // Add and enter password
-  var password = passwordBlank.join("");
-  console.log("Your Pasword is: " + password);
-  return password;
-  
+
+//user prompt to select type
+
+    let useSpecial = false; 
+    let useNumerical = false; 
+    let useLower = false; 
+    let useUpper = false; 
+
+    while (!useSpecial && !useNumerical && !useLower && !useUpper) {
+        alert("You need to select at least one character type for your password from the options following this prompt.");
+        useSpecial = confirm("Would you like to have special characters in your password?"); 
+        useNumerical = confirm("Would you like to have numerical characters in your password?"); 
+        useLower = confirm("Would you like to have lowercase letters in your password?"); 
+        useUpper = confirm("Would you like to have uppercase letters in your password?"); 
+
+    }
+      
+
+//generate list of characters to use in password based on user selection
+
+    function addCharSelect(useChar,type) {
+        if (useChar) {
+            passwordOptions = passwordOptions + type;
+            passwordCheck.push(type); 
+        }; 
+    }
+
+    addCharSelect(useSpecial, special);
+    addCharSelect(useNumerical, numerical); 
+    addCharSelect(useLower, alphaLower);
+    addCharSelect(useUpper, alphaUpper); 
+
+    // if (useSpecial) {
+    //     passwordOptions = passwordOptions + special;
+    //     passwordCheck.push(special); 
+    //     }; 
+    // if (useNumerical) {
+    //     passwordOptions = passwordOptions + numerical;
+    //     passwordCheck.push(numerical); 
+    //     }; 
+    // if (useLower) {
+    //     passwordOptions = passwordOptions + alphaLower;
+    //     passwordCheck.push(alphaLower); 
+    //     }; 
+    // if (useUpper) {
+    //     passwordOptions = passwordOptions + alphaUpper;
+    //     passwordCheck.push(alphaUpper); 
+    //     }; 
+
+
+
+//function to generate random interger in range 0 to last index of a list lenght:
+
+
+    function randomNum() {
+        return Math.floor(Math.random()*(passwordOptions.length)); 
+    }
+
+    // let stringLength= passwordOptions.length;
+
+//generate password of given lenght form list of possible characters
+    function generateNewPassword(){ 
+        passwordGenerated = '';
+        for (i=0; i < numberChar; i++) {
+            passwordGenerated= passwordGenerated + passwordOptions[randomNum()];
+        }
+        console.log("Generated password is " + passwordGenerated);
+    }
+
+
+
+    generateNewPassword(); 
+     
+
+   
+    
+    function checkGeneratedPassword (){ 
+        let countCharCheck = 0; 
+        let totalCountCharCheck =[]; 
+
+        function checkNamedChar(useChar, type) {
+            if(useChar) {
+                countCharCheck = 0;
+                for (i=0; i < passwordGenerated.length; i++) {
+                    if (type.indexOf(passwordGenerated[i]) !== -1) {
+                        countCharCheck++; 
+                    }
+                }
+                totalCountCharCheck.push(countCharCheck);  
+            }
+        }
+    
+        checkNamedChar(useSpecial, special);
+        checkNamedChar(useNumerical, numerical);
+        checkNamedChar(useLower, alphaLower);
+        checkNamedChar(useUpper, alphaUpper); 
+
+        console.log(totalCountCharCheck); 
+
+        while (totalCountCharCheck.indexOf(0) !== -1) {
+            totalCountCharCheck =[];
+            generateNewPassword(); 
+            checkGeneratedPassword(); 
+        }
+    }
+
+    checkGeneratedPassword(); 
+
 }
